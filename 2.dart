7c074d7d78411891d53a36dcf0da3232cae47708
munawar3206 <munawarkuttimon68@gmail.com>
2023-09-26 14:13:33 +0530

    how much year to reach 50

---
+++ 2.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+void main() {
+  stdout.write('Name :');
+  String? name = stdin.readLineSync();
+
+  stdout.write('Age');
+  int age = int.parse(stdin.readLineSync()!);
+
+  int toreach50 = 50 - age;
+  print(
+      'Your name is $name , you are $age old , You have $toreach50 more to reach 50 year old');
+}
