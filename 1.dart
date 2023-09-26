0399cef4ef87800a6879301c950f93b1e32133be
munawar3206 <munawarkuttimon68@gmail.com>
2023-09-26 13:59:10 +0530

    first commit

---
+++ 1.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+void main() {
+  stdout.write('Name : ');
+  String? name = stdin.readLineSync();
+  print("Hi $name");
+  stdout.write('Qualification : ');
+  String? quali = stdin.readLineSync();
+  stdout.write('Current Qualification : ');
+  String? more = stdin.readLineSync();
+   stdout.write('Since : ');
+  int a = int.parse(stdin.readLineSync() ?? '');
+  print('Hi $name, i think your qualification $quali.Are u know $more.SINCE $a');
+}
