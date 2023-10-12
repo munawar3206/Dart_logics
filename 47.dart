470d0f253a09d24163de11875f48687d5615f398
munawar3206 <munawarkuttimon68@gmail.com>
2023-10-12 17:16:11 +0530

    logics

--- 47.dart
+++ 47.dart
@@ -1,14 +1,18 @@
+import 'dart:io';
+
 void main() {
-  int a = 10;
-  int b = 5;
-  int c = 50;
+  stdout.write('Enter the First number : ');
+  int a = int.parse(stdin.readLineSync()!);
+  stdout.write('Enter the Second number : ');
+  int b = int.parse(stdin.readLineSync()!);
+  stdout.write('Enter the Third number : ');
+  int c = int.parse(stdin.readLineSync()!);
   if (a > c && a > b) {
     print(a);
-  } else if 
-     (b > c && b > a) {
-      print(b);
-    }else {
-      print(c);
-    }
+  } else if (b > c && b > a) {
+    print(b);
+  } else {
+    print(c);
   }
+}
 
